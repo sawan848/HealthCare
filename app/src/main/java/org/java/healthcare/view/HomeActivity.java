9edb07b2589eec5e796ac24ab71cdfb8c0fdf3dd
sawan848 <ksawan802@gmail.com>
2022-06-16 20:49:01 +0530

    modified some codes

--- app/src/main/java/org/java/healthcare/view/HomeActivity.java
+++ app/src/main/java/org/java/healthcare/view/HomeActivity.java
@@ -1,13 +1,11 @@
 package org.java.healthcare.view;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.button.MaterialButton;
 
 import org.java.healthcare.R;
 
@@ -19,13 +17,8 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         doctor=findViewById(R.id.doctorBtn);
-        doctor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(HomeActivity.this, "This is my Toast message!",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
+        doctor.setOnClickListener(v -> Toast.makeText(HomeActivity.this, "This is my Toast message!",
+                Toast.LENGTH_LONG).show());
 
     }
 }
