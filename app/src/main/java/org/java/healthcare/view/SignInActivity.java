28a9e9df4dcd0edbd236776b6f022e9ab7a7211e
Manisha-Arukala <manishaarukala06@gmail.com>
2022-12-21 14:32:28 +0530

    splash and sign is created

--- app/src/main/java/org/java/healthcare/view/SignInActivity.java
+++ app/src/main/java/org/java/healthcare/view/SignInActivity.java
@@ -1,13 +1,16 @@
 package org.java.healthcare.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import org.java.healthcare.R;
 
 public class SignInActivity extends AppCompatActivity {
-
+    AppCompatButton registerBtn,loginBtn;
+    EditText emailText,passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
