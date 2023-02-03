956a346cc28fb617affcb5e79c1bdba45f267b0f
Sawan Kumar <ksawan802@gmail.com>
2023-02-03 18:37:03 +0530

    modules added

--- app/src/main/java/org/java/healthcare/Activity/DoctorActivity.java
+++ app/src/main/java/org/java/healthcare/Activity/DoctorActivity.java
@@ -2,7 +2,10 @@ package org.java.healthcare.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.java.healthcare.R;
 
@@ -12,5 +15,17 @@ public class DoctorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
+
+        ImageView imageViewDoc1=findViewById(R.id.imageViewDoc2);
+        TextView textViewDoc1=findViewById(R.id.textViewDoc4);
+
+        imageViewDoc1.setOnClickListener(view -> {
+            startActivity(new Intent(DoctorActivity.this,DoctorProfileActivity.class));
+
+        });
+
+        textViewDoc1.setOnClickListener(view -> {
+            startActivity(new Intent(DoctorActivity.this,SignInActivity.class));
+        });
     }
 }
---
+++ app/src/main/java/org/java/healthcare/Activity/DoctorProfileActivity.java
@@ -0,0 +1,16 @@
+package org.java.healthcare.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.java.healthcare.R;
+
+public class DoctorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+    }
+}
